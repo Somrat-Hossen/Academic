5bb9852f49a74a2e76c8454f9ae4462007133d30
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-06-29 18:59:13 +0600

    Update Gauss seidel.cpp

--- Numerical Method/Gauss seidel.cpp
+++ Numerical Method/Gauss seidel.cpp
@@ -1,26 +1,19 @@
- include<bits/stdc++.h>
-using namespace std;
+ #include<bits/stdc++.h>
+ using namespace std;
 
-int main()
+double arr[100][100] , x[100] = {0};
+
+
+void Compute(int n)
 {
-    double arr[100][100] , x[100] = {0};
-    int n,i,j,t=1;
-    cin>>n;
-    for( int i = 1; i <= n; i++ )
+   for(int t=1;t<=50;t++)
     {
-        for( int j = 1; j <= n+1; j++ )
+        for(int i=1;i<=n;i++ )
         {
-            cin>>arr[i][j];
-        }
-    }
-    for( t=1; t<=50; t++)
-    {
-        for( i = 1; i <= n; i++ )
-        {
-            double sum = 0;
-            for( j = 1; j <= n; j++ )
+            double sum=0;
+            for(int j =1; j <= n; j++ )
             {
-                if( i != j ) ///ramaining unchange the ith and jth row-col
+                if( i != j ) ///ramained  unchange the ith and jth cell
                 {
                     sum +=  ( arr[i][j] * x[j] );
                 }
@@ -30,12 +23,28 @@ int main()
             x[i] = sum;
         }
     }
+}
+int main()
+{
+    int n;
+    cin>>n;
 
-    cout<<endl;
-
-    for( i = 1; i <= n; i++)
+    for( int i=1;i<=n;i++ )
     {
-                                                                                                  cout<<x[i]<<" ";
+        for( int j =1;j <=n+1;j++ )
+        {
+            cin>>arr[i][j];
+        }
     }
-    return 0;
+
+
+    Compute(n);
+
+    for(int i = 1; i <= n; i++)
+    {
+      cout<<x[i]<<" ";
+    }
+
+
+   return 0;
 }
