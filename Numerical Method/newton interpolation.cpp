df9f7693cc41cab998b0a932e1c892491dd9f952
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-07-05 01:11:05 +0600

    Add files via upload

--- Numerical Method/newton interpolation.cpp
+++ Numerical Method/newton interpolation.cpp
@@ -1,77 +1,57 @@
 #include<bits/stdc++.h>
+#define mx 1000
 using namespace std;
 
-// ====================================================================
 int n,m;
-double xx[100],yy[100],a[100];
-double dp[100][100]={0};
 
-/*
-double funtion(int tmp)
-{
-        double a=tmp;
-        return pow(a,5)+(6*pow(a,4))-(2*pow(a,3))+ 4;
-}
-int main()
-{
-        freopen("in.txt","w",stdout);
-        ios::sync_with_stdio(0);
-        cin.tie(0);
-        int i,j,t,tcs(0),len,tmp,k,u,v,cnt(0),mx=INT_MIN,mn=INT_MAX;
+   class data
+   {
+       public:
+       double x,y;
 
-        cout<<5<<endl;
-        for(i=0;i<=8;i++)
+   }arr[mx]; ///array of pointer for x and f(x) as for input
+
+   double fun(int r,int l) ///start and end
+   {
+        if(r==l) return arr[r].y;
+
+        if(r+1==l)
         {
-                tmp=rand()%10;
-                cout<<tmp<<' '<<precise(5)<<fun(tmp)<<endl;
+                return ((arr[l].y-arr[r].y)/(arr[l].x-arr[r].x));
         }
-        tmp=5;
-cout<<tmp<<' '<<precise(5)<<fun(tmp)<<endl;
-	return 0;
-}
-*/
+        return (fun(r+1,l)-fun(r,l-1) )/(arr[l].x-arr[r].x);
+   }
 
-
-
-double fun(int start,int endd)
-{
-        if(start==endd) return yy[start];
-        if(start+1==endd)
-        {
-                return ((yy[endd]-yy[start])/(xx[endd]-xx[start]));
-        }
-        return (fun(start+1,endd)-fun(start,endd-1) )/(xx[endd]-xx[start]);
-}
-
-double fun2(int i,double x)
-{
+   double fun2(int i,double x)
+   {
         double tmp=1;
+
         for(int j=0;j<i;j++)
         {
-                tmp*=(x-xx[j]);
+                tmp*=(x-arr[j].x);
         }
         return tmp;
-}
+   }
 
 int main()
 {
-        int i,j;
+     int i,j;
 
-        cin>>n;
-        for(i=0;i<n;i++)
-        {
-                cin>>xx[i]>>yy[i];
-        }
+     cin>>n;
+     for(i=0;i<n;i++)
+     {
+             cin>>arr[i].x>>arr[i].y;  ///x and f(x) as input
+     }
 
-        double x,f_of_x=0;
-        cin>>x;
-        for(i=1;i<=n;i++)
-        {
-                //DEBUG(f_of_x,fun(0,i),fun2(i,x));
-                f_of_x+=(fun(0,i)*fun2(i,x));
-        }
+     double x,f_of_x=0;
+     cin>>x;
 
-        cout<<f_of_x+yy[0]<<endl;
+     for(i=1;i<=n;i++)
+     {
+       f_of_x+=(fun(0,i)*fun2(i,x));
+     }
+
+     cout<<f_of_x+arr[0].y<<endl;
 
 	return 0;
 }
