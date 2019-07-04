feff590f7155e275d76fd110cd6f5ea3eb7fe596
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2019-07-05 00:54:21 +0600

    Update Jacobi Method.cpp

--- Numerical Method/Jacobi Method.cpp
+++ Numerical Method/Jacobi Method.cpp
@@ -2,40 +2,41 @@
 #define mx 100
 using namespace std;
 
-double arr[mx][mx], x[mx]; ///global array contains 0 as garbage value
+double arr[mx][mx], res[mx],tmp[mx]; ///global array contains 0 as garbage value
 double error=0.0001,relative_err,mx_err;
 
 void Compute(int n)
 {
-   for(int t=1;t<=50;t++)
-    {
-       mx_err=0.0;
-        for(int i=1;i<=n;i++ )
+   int i,k,j;
+
+   for(int it=1;it<=4;it++) ///keep track of the num of iteration
+   {
+        for(i=0;i<n;i++ )
         {
             double sum=0;
-            for(int j =1; j <= n; j++ )
+            for( j =0;j<n;j++ )
             {
                 if(i!=j ) ///ramained  unchange the ith and jth cell
                 {
-                    sum += (arr[i][j] * x[j]);
+                    sum += (arr[i][j] * res[j]);
                 }
             }
-            sum = ( arr[i][n+1] - sum);
-            sum = sum / arr[i][i];
-
-            relative_err=fabs((x[i]-sum)/sum);///This error finding part is advanced
-            if(relative_err>mx_err)
-            {
-               mx_err=relative_err;
-            }
-
-            x[i] = sum;
+            tmp[i]=(arr[i][n]-sum)/arr[i][i];  ///Store the present result in a temporary array.
         }
+
+        /*As the method describe that the previous computed result will be used in the next state ,so i did not change the
+        initial result in this step ,after computed one step i assign the result in the final array.
+        */
+      for( k=0;k<n;k++) res[k]=tmp[k]; ///Replace the present result in our res array.
+
+      cout<<"Iteration "<<it<<":  ";
+      for( k=0;k<n;k++) cout<<"X"<<k+1<<"="<<res[k]<<", ";
+      cout<<endl;
     }
 
-    if(mx_err>error)
+    /*if(mx_err>error)
     {
-       puts("Error is not compatible with the given error:");
+       puts("Error is not compatible with the given error between this iteration:");
        return;
     }
     else
@@ -44,16 +45,18 @@ void Compute(int n)
        {
          cout<<fixed<<setprecision(4)<<x[i]<<" ";
        }
-    }
+    }*/
 }
 int main()
 {
+    ///freopen("in.txt","r",stdin);
+
     int n;
     cin>>n;
 
-    for( int i=1;i<=n;i++ )
+    for( int i=0;i<n;i++ )
     {
-        for( int j =1;j <=n+1;j++ )
+        for( int j=0;j<=n;j++ )
         {
             cin>>arr[i][j];
         }
@@ -64,4 +67,5 @@ int main()
 
    return 0;
 }
+
 
